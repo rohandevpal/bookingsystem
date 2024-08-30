94d115f54ce2af6f6b1ed82ce9567a1c403165a7
rohandevpal <devrohanpal@gmail.com>
2024-08-30 12:46:55 +0530

    first changes

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    fontSize: {
+      "2xsm": "10px",
+      xsm: "12px",
+      sm: ["14px", { lineHeight: "20px" }],
+      base: ["16px", { lineHeight: "24px" }],
+      lg: ["18px", { lineHeight: "28px" }],
+      xl: ["20px", { lineHeight: "32px" }],
+      "2xl": "22px",
+      "3xl": "25px",
+      "4xl": "32px",
+      "5xl": "40px",
+      "6xl": "48px",
+      "7xl": "70px",
+    },
+  },
+  plugins: [],
+};
+export default config;
