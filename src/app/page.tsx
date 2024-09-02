d9b7325c28cfe281de4e49456596a23e8f789058
rohandevpal <devrohanpal@gmail.com>
2024-09-02 12:42:10 +0530

    api data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,31 @@ import NavBar from "./component/Navbar";
 import Header from "./component/Header";
 import Card from "./component/card";
 
-export default function Home() {
+
+async function getData(){
+  const response = await fetch('https://api.github.com/repositories/471339241');
+  const data = await response.json();
+  return data;
+}
+
+async function getTime() {
+    const response = await fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata',{
+      next:{
+        revalidate: 3,
+      }
+    });
+    const date = await response.json();
+    return date;
+}
+
+export default async function Home() {
+  const [data, time] = await Promise.all([getData(), getTime()]);
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {/* <p>{data.full_name}</p>
+        <p>{time.datetime}</p> */}
         <Card />
       </div>
     </main>
