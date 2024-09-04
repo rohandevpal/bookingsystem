5e8a173bce382770191a340852b11d79c6170bda
rohandevpal <devrohanpal@gmail.com>
2024-09-04 12:24:47 +0530

    run uri

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
         <main className="bg-gray-100 min-h-screen w-screen">
           <main className="max-w-screen-2xl m-auto bg-white">
             <NavBar />
-            <p>{data.full_name}</p>
+
             {children}
           </main>
         </main>
